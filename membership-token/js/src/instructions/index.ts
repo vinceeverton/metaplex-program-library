eae59ab2cc4b1144b46ffc40ce31a44a1fd38b59
Michael Sebastiyan <sebastiyan.michael@outlook.com>
2022-02-03 11:04:33 +0200

    feat: add withdraw and claim

--- membership-token/js/src/instructions/index.ts
+++ membership-token/js/src/instructions/index.ts
@@ -1,5 +1,7 @@
 export * from './buy';
+export * from './claimResource';
 export * from './closeMarket';
 export * from './createMarket';
 export * from './createStore';
 export * from './initSellingResource';
+export * from './withdraw';
